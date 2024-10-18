1c869aa18f0fd4b5a5cf0538bce3bd59297a1896
Kishorgujar <91127600+Kishorgujar@users.noreply.github.com>
2024-10-18 17:40:27 +0530

    Update variables.tf

--- modules/iam_role/variables.tf
+++ modules/iam_role/variables.tf
@@ -1,30 +1,24 @@
 variable "eks_role_name" {
-  description = "The name of the EKS role."
+  description = "The name of the EKS role"
   type        = string
-  default     = "EKSRole"
 }
 
 variable "eks_role_tag" {
-  description = "The tag for the EKS role."
+  description = "Tag for the EKS role"
   type        = string
-  default     = "EKS"
 }
 
 variable "node_role_name" {
-  description = "The name of the Node role."
+  description = "The name of the Node role"
   type        = string
-  default     = "EKSNodeRole"
 }
 
 variable "node_role_tag" {
-  description = "The tag for the Node role."
+  description = "Tag for the Node role"
   type        = string
-  default     = "NodeGroup"
 }
+
 variable "load_balancer_controller_policy_name" {
-  description = "The name of the AWS Load Balancer Controller IAM Policy"
+  description = "The name for the Load Balancer Controller IAM policy"
   type        = string
-  default     = "AWSLoadBalancerControllerIAMPolicy"
 }
-
-
